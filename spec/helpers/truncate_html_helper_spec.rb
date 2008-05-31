fbf7044500ec6707b241779d8a4c7de36876944c
Ian White <ian.w.white@gmail.com>
2008-05-31 12:14:21 +0100

    Fixed typo

--- spec/helpers/truncate_html_helper_spec.rb
+++ spec/helpers/truncate_html_helper_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
 
-describe Ardes::TruncateHtmlHelper
+describe Ardes::TruncateHtmlHelper do
   include Ardes::TruncateHtmlHelper
   
   def self.with_length_should_equal(n, str)
