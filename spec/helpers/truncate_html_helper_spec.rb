5e2b29ed77b580848b9c33e47c71191f21abc50f
Ian White <ian.w.white@gmail.com>
2008-05-31 12:13:16 +0100

    Updated spec for recent rspec changes

--- spec/helpers/truncate_html_helper_spec.rb
+++ spec/helpers/truncate_html_helper_spec.rb
@@ -1,48 +1,35 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
 
-module TruncateHtmlHelperSpecHelper
-  def self.included(base) 
-    base.class_eval do
-      def self.with_length_should_equal(n, str)
-        it "#{n}, should equal #{str}" do
-          truncate_html(@html, n).should == str
-        end
-      end
+describe Ardes::TruncateHtmlHelper
+  include Ardes::TruncateHtmlHelper
+  
+  def self.with_length_should_equal(n, str)
+    it "#{n}, should equal #{str}" do
+      truncate_html(@html, n).should == str
     end
   end
-end
-
-describe Ardes::TruncateHtmlHelper, "html: <p>Hello <strong>World</strong></p>, length: " do
-  include TruncateHtmlHelperSpecHelper
   
-  before do
-    @html = '<p>Hello <strong>World</strong></p>'
+  
+  describe "html: <p>Hello <strong>World</strong></p>, length: " do
+    before { @html = '<p>Hello <strong>World</strong></p>' }
+  
+    with_length_should_equal 3, '<p>Hel&hellip;</p>'
+    with_length_should_equal 7, '<p>Hello <strong>W&hellip;</strong></p>'
+    with_length_should_equal 11, '<p>Hello <strong>World</strong></p>'
   end
   
-  with_length_should_equal 3, '<p>Hel&hellip;</p>'
-  with_length_should_equal 7, '<p>Hello <strong>W&hellip;</strong></p>'
-  with_length_should_equal 11, '<p>Hello <strong>World</strong></p>'
-end
+  describe 'html: <p>Hello &amp; <span class="foo">Goodbye</span> <br /> Hi</p>, length: ' do
+    before { @html = '<p>Hello &amp; <span class="foo">Goodbye</span> <br /> Hi</p>' }
 
-describe Ardes::TruncateHtmlHelper, 'html: <p>Hello &amp; <span class="foo">Goodbye</span> <br /> Hi</p>, length: ' do
-  include TruncateHtmlHelperSpecHelper
-  
-  before do
-    @html = '<p>Hello &amp; <span class="foo">Goodbye</span> <br /> Hi</p>'
+    with_length_should_equal 7, '<p>Hello &amp;&hellip;</p>'
+    with_length_should_equal 9, '<p>Hello &amp; <span class="foo">G&hellip;</span></p>'
+    with_length_should_equal 18, '<p>Hello &amp; <span class="foo">Goodbye</span> <br></br> H&hellip;</p>'
   end
-  
-  with_length_should_equal 7, '<p>Hello &amp;&hellip;</p>'
-  with_length_should_equal 9, '<p>Hello &amp; <span class="foo">G&hellip;</span></p>'
-  with_length_should_equal 18, '<p>Hello &amp; <span class="foo">Goodbye</span> <br></br> H&hellip;</p>'
-end
 
-describe Ardes::TruncateHtmlHelper, ' (incorrect) html: <p>Hello <strong>World</p><div>And Hi, length: ' do
-  include TruncateHtmlHelperSpecHelper
+  describe '(incorrect) html: <p>Hello <strong>World</p><div>And Hi, length: ' do
+    before { @html = '<p>Hello <strong>World</p><div>And Hi' }
   
-  before do
-    @html = '<p>Hello <strong>World</p><div>And Hi'
+    with_length_should_equal 10, '<p>Hello <strong>Worl&hellip;</strong></p>'
+    with_length_should_equal 30, '<p>Hello <strong>World</strong></p><div>And Hi</div>'
   end
-  
-  with_length_should_equal 10, '<p>Hello <strong>Worl&hellip;</strong></p>'
-  with_length_should_equal 30, '<p>Hello <strong>World</strong></p><div>And Hi</div>'
 end
