1a36b090e737378f4075e61986744f4c02e6339d
Ian White <ian@abraxis.local>
2009-02-15 11:47:56 +0000

    Fix broken spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,15 +2,15 @@ ENV["RAILS_ENV"] ||= "test"
 __DIR__ = File.dirname(__FILE__)
 
 # if we're in a rails env, use that, otherwise use rubygems to create a spec env
-if File.exist?(rails_env = "#{__DIR__}/../../../../config/environment")
-  require rails_env
-else
+begin
+  require "#{__DIR__}/../../../../config/environment"
+rescue LoadError
   require 'rubygems'
   require 'activesupport'
   require 'actionpack'
   require 'action_view'
-  $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
-  require File.join(File.dirname(__FILE__), '..', 'init')
+  $LOAD_PATH << "#{__DIR__}/../lib"
+  require "#{__DIR__}/../init"
 end
 
 require 'spec'
