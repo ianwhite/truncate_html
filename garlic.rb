60607c981146b86923e4ce91e1d8faf4f520ab05
Ian White <ian.w.white@gmail.com>
2009-04-21 08:58:38 +0100

    Adding just one target to garlic for testing purposes

--- garlic.rb
+++ garlic.rb
@@ -5,10 +5,10 @@ garlic do
   repo 'rspec', :url => 'git://github.com/dchelimsky/rspec'
   repo 'rspec-rails', :url => 'git://github.com/dchelimsky/rspec-rails'
 
-  ['master', '2-3-stable', '2-2-stable', '2-1-stable'].each do |rails|
+  ['2-3-stable'].each do |rails|
     target rails, :tree_ish => "origin/#{rails}" do
       prepare do
-        plugin 'truncate_html', :clone => true
+        plugin 'truncate_html'
         plugin 'rspec'
         plugin 'rspec-rails' do
           `script/generate rspec -f`
