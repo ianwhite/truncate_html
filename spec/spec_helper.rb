c81b39962478bbcc8a631732e45a55a108665811
Deshi Xiao <xiaods@gmail.com>
2011-12-23 15:15:55 +0800

    upgrade to rspec2 and bundle

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,18 +3,11 @@ __DIR__ = File.dirname(__FILE__)
 
 require 'rubygems'
 
-# if we're in a rails env, use that, otherwise use rubygems to create a spec env
-begin
-  require "#{__DIR__}/../../../../config/environment"
-rescue LoadError
-  require 'activesupport'
-  require 'actionpack'
-  require 'action_view'
-  $LOAD_PATH << "#{__DIR__}/../lib"
-  require "#{__DIR__}/../init"
-end
+require 'rails/all'
+$LOAD_PATH << "#{__DIR__}/../lib"
+require "#{__DIR__}/../init"
 
-require 'spec'
+require 'rspec'
 
 # dependencies of truncate_html
 require 'htmlentities'
